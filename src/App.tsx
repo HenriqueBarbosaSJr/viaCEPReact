29eb904763ef09b02bf125a8de0f8934ab4023ef
Henrique Barbosa de Souza Júnior <henrique@hbveterinaria.com.br>
2024-06-09 23:51:22 -0300

    Ponto inicial do projeto

---
+++ src/App.tsx
@@ -0,0 +1,37 @@
+
+import { useState } from 'react';
+import './App.css'
+import api from './services/api'
+
+function App() {
+
+  const [ searchCep, setSearchCep] = useState('');
+  const [ cep, setCep] = useState('');
+  const [ uf, setUf] = useState('');
+  const [ log, setLog] = useState('');
+  const [ bairro, setBairro] = useState('');
+
+
+  async function search(){
+    const resp = await api.get(searchCep + '/json/');
+    console.log(resp.data.bairro);
+    setCep(resp.data.cep);
+    setUf(resp.data.uf);
+    setLog(resp.data.logradouro);
+    setBairro(resp.data.bairro);
+  }
+
+  return (
+    <>
+      <p>Consulta ViaCEP</p>
+      <input type="text" onChange={e => setSearchCep(e.target.value)} /><br/><br/>
+      <button onClick={search}>Consultar CEP</button>
+      <p>CEP: {cep}</p>
+      <p>UF: {uf}</p>
+      <p>Logradouro: {log}</p>
+      <p>Bairro: {bairro}</p>
+    </>
+  )
+}
+
+export default App
